6af58ba885f518b176e8c29b3b912cd6c68b6eef
Mariel Juliene Macabudbud <arkadx213@gmail.com>
2024-05-21 19:29:42 +0800

    fetch products from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
 import { Link, useParams } from "react-router-dom";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductScreen = () => {
+	const [product, setProduct] = useState({});
 	const { id: productId } = useParams();
-	const product = products.find((p) => p._id === productId);
-	console.log(product);
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${productId}`);
+			setProduct(data);
+		};
+
+		fetchProduct();
+	}, [productId]);
 
 	return (
 		<>
